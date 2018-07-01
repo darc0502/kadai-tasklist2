4cdabacc8b16b1a397b811ea0e007027b7d5e7b9
darc <darc_ruby@yahoo.co.jp>
2018-07-01 12:39:06 +0000

    saiteisyutu

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180630125134) do
+ActiveRecord::Schema.define(version: 20180605003318) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string   "status"
     t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id", using: :btree
   end
 
